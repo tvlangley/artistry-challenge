01a98c2a79463767a8adcf5e9f73c55d3e2a5fc5
tvlangley <tonia.langley@gmail.com>
2021-04-12 13:22:05 -0400

    solution

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,2 +1,13 @@
 class Artist < ApplicationRecord
-end
+    has_many :artist_instruments
+    has_many :instruments, through: :artist_instruments
+  
+    # VALIDATING: 
+    # name --> presence 
+    # title --> uniqueness
+    # age --> greater than 90
+  
+    validates :name, presence: true
+    validates :title, uniqueness: true
+    validates :age, numericality: {greater_than: 90}
+  end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   
+  resources :artist_instruments, only: [:new, :create]
+  resources :artists, only: [:show, :index, :new, :create]
+  resources :instruments, only: [:index]
+
+  # get '/artist_instruments'
+  # post '/artist_instruments'
 end
